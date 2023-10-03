692f0a919c9176eb3a0a3a869e3030912e4c9482
Ronnie <romagifted@gmail.com>
2023-10-04 01:44:26 +0700

    fixed

--- __build/data/transport.core.usergroups.php
+++
@@ -1,8 +0,0 @@
-<?php
-use MODX\Revolution\modUserGroup;
-$collection['1']= $xpdo->newObject(modUserGroup::class);
-$collection['1']->fromArray([
-  'id' => 1,
-  'name' => 'Administrator',
-  'parent' => 0,
-], '', true, true);
